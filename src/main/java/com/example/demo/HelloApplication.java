874df9f6f2df9db21bca7a75dcdd8286438936a1
SoulHunter <erokhin.33@yandex.ru>
2022-11-12 02:12:18 +0300

    xz

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -13,10 +15,14 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-
-        stage.setTitle("Погода");
+        scene.setFill(Color.TRANSPARENT);
+        stage.setTitle("Weather");
         stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setResizable(false);
+        ((HelloController)fxmlLoader.getController()).initialize(stage);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,8 +1,73 @@
 package com.example.demo;
 
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.ResourceBundle;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class HelloController {
 
-}
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView Close;
+
+    @FXML
+    private ImageView Minimize;
+
+    @FXML
+    private AnchorPane UpPane;
+    private double x, y;
+
+    @FXML
+    private Label time;
+
+    private int minute;
+    private int hour;
+    private int second;
+
+    @FXML
+    void initialize(Stage stage) {
+        UpPane.setOnMousePressed(mouseEvent -> {
+            x = mouseEvent.getSceneX();
+            y = mouseEvent.getSceneY();
+        });
+        UpPane.setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX() - x);
+            stage.setY(mouseEvent.getScreenY() - y);
+        });
+
+        Close.setOnMouseClicked(mouseEvent -> stage.close());
+        Minimize.setOnMouseClicked(mouseEvent -> stage.setIconified(true));
+
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e ->
+                time.setText(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss")))
+        ),
+                new KeyFrame(Duration.seconds(1))
+        );
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
+    }
+
+
+
+
+
